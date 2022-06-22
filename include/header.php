77e6cc1aea06cbf96d8636d252ee5e2fee067519
Shubham <shubhamkumar.123.sk001@gmail.com>
2022-06-22 13:39:52 +0530

    init

---
+++ include/header.php
@@ -0,0 +1,50 @@
+<div class="header sticky-top">
+    <nav class="navbar navbar-expand-md navbar-light">
+        <a class="navbar-brand" href="#">
+            <img src="img/logo.png" />
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#my-navbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="my-navbar" style="background-color: rgb(45, 45, 45); padding-left: inherit;">
+            <ul class="navbar-nav" style="font-size: large; font-weight: 500;">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">
+                        <i class="fa fa-home"></i>Home
+                    </a>
+                </li>
+
+                <div class="nav-vl"></div>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa fa-info"></i>About Us
+                    </a>
+                </li>
+
+                <div class="nav-vl"></div>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa fa-tasks"></i>Program
+                    </a>
+                </li>
+
+                <div class="nav-vl"></div>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa fa-hand-o-right" aria-hidden="true"></i>Join Us
+                    </a>
+                </li>
+
+                <div class="nav-vl"></div>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact_us.php">
+                        <i class="fa fa-address-book"></i>Contact Us
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+</div>
---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="icon" href="img/logo-icon.png">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="theme-color" content="#000000">
+    <meta name="description" content="Suvidha Mahila Mandal is a non-profit organization working to impart education among the financially challenged sections to help them realize parity in education and strength of little minds in building a promising future.">
+    <meta name="keywords" content="internshala, free internship, free internship for students, graduate internship, internship, women, women empowerment, Entrepreneurship, research">
+    <title>Suvidha Foundation</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" rel="stylesheet" />
+    <link href="css/header.css" rel="stylesheet">
+    <link href="css/video.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <?php
+    include "include/header.php";
+    ?>
+    <div>
+        <?php
+        include "slider.php";
+        ?>
+    </div>
+    <!-- Suvidha Foundation -->
+    <div class="video-left row">
+        <div class="col-8 row card" style="margin: 0px auto 20px!important;">
+            <div class="col-4">
+                <!-- <iframe id="intro" width="560" height="315" src="https://www.youtube.com/embed/GcCgwhRHMdU?rel=0?version=3&amp;autoplay=0&amp;controls=0&amp;&amp;showinfo=0&amp;loop=1&amp;mute=0&amp;playlist=GcCgwhRHMdU" frameborder="0" allow="autoplay"></iframe> -->
+            </div>
+            <div class="col-8">
+                <div class="para-title">
+                    Suvidha Foundation!
+                </div>
+                <div>
+                    <hr class="div-hr" />
+                </div>
+                <div class="video-content">
+                    <p> Suvidha Mahila Mandal is a non-profit organization working to impart education among the financially challenged sections to help them realize parity in education and strength of little minds in building a promising future. The organization has provisions of student internships, student mentorship and the scope to volunteer. Through these programmes, the organization aims to achieve the vision of imparting innovative education that stays with the students forever and equip them for the unforeseen future.</p>
+                </div>
+                <div>
+                    <a class="fancy" href="#">
+                        <span class="top-key"></span>
+                        <span class="text">Know More</span>
+                        <span class="bottom-key-1"></span>
+                        <span class="bottom-key-2"></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Our Programs -->
+    <div>
+        <div class="para-title" style="padding-top:2em;">
+            <span>Our Programs</span>
+        </div>
+        <div>
+            <hr class="div-hr" style="width: 25em; background: black;" />
+        </div>
+        <div class="normal-card col-10">
+            <div class="justify-content-between row">
+                <div class="img-item">
+                    <img src="img/mentorshipIcon.png" class="program-image">
+                    <span class="img-alt">Student Mentorship</span>
+                </div>
+                <div class="img-item">
+                    <img src="img/scholarshipIcon.jpeg" class="program-image">
+                    <span class="img-alt">Student Internship</span>
+                </div>
+                <div class="img-item">
+                    <img src="img/workshop.png" class="program-image">
+                    <span class="img-alt">Free Workshop for School</span>
+                </div>
+                <div class="img-item">
+                    <img src="img/WomenEmpIcon.jpeg" class="program-image">
+                    <span class="img-alt">Women Empowerment</span>
+                </div>
+                <div class="img-item">
+                    <img src="img/scholarshipIcon.jpeg" class="program-image">
+                    <span class="img-alt">Student Scholarship</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- why donate! -->
+    <div style="margin-top: 8em; background-image:url(https://images.unsplash.com/photo-1544776193-32d404ae608a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8cG9vcmtpZCxjaGlsZHJlbnx8fHx8fDE2NTUyMDg0NTY&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600) ">
+        <div class="normal-card col-8 img-alt" style="font-weight: normal;">
+            <div>
+                <h2 style="text-align: center;">Why Should You Donate</h2>
+            </div>
+            <div>
+                <hr class="div-hr" style="width: 25em; background: black;" />
+            </div>
+            <div>
+                <p>“We make a living by what we get. We make a life by what we give.”
+                    <br />
+                    - Winston Churchill
+                </p>
+                <p>
+                    Our programmes are curated to encourage innovation and to teach exceptional approach because no problem is identical. No child is born equal but can certainly aspire to be one because we believe once we inspire one, the inspiration can motivate all to dream beyond the barriers of finance and help build a better, brighter society ahead for everyone.
+                    <br /><br />
+                    To realize this vision, the children need you. They need your contribution to make a real change and you can contribute to bring that change now.
+                    <br /><br />
+                    Donate for the cause you truly care!
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <!-- our partner -->
+    <div>
+        <div class="para-title" style="padding-top:2em;">
+            <span>Our Partners</span>
+        </div>
+        <div>
+            <hr class="div-hr" style="width: 25em; background: black;" />
+        </div>
+        <div class="pic-ctn">
+            <img src="img/CodeKaroYaro.jpeg" class="pic">
+            <img src="img/CodeKaroYaro.jpeg" class="pic">
+            <img src="img/CodeKaroYaro.jpeg" class="pic">
+            <img src="img/CodeKaroYaro.jpeg" class="pic">
+            <img src="img/CodeKaroYaro.jpeg" class="pic">
+        </div>
+    </div>
+    <!-- footer -->
+    <div class="footer-body">
+        <div class="footer-top">
+            <spam class="footer-title">Register Your School For Free Online Coding Webinar And
+                <br />
+                Prepare Your Kids For Indian Computing Olympiad
+            </spam>
+            <a class="fancy" href="#">
+                <span class="top-key"></span>
+                <span class="text">Register Now</span>
+                <span class="bottom-key-1"></span>
+                <span class="bottom-key-2"></span>
+            </a>
+        </div>
+    </div>
+
+</body>
+
+</html>
